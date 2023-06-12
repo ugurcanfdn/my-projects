703e28a102c7e799caff133e83d95f5bb24454dc
UGURCAN <ugurcanfdn@gmail.com>
2023-06-12 20:17:49 +0300

    sakggskakg

---
+++ projects/projects/Project-101/Third_Part/backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if we are root privilage or not
+
+# Define the backup directory
+
+# Define the directories to be backed up
+
+# Get the hostname of the instance
+
+# Get the current date and time
+
+# Print start status message.
+
+# Create the backup directory if it doesn't exist
+
+# Iterate through the directories to be backed up
+
+# Or if you have only one directory that you know.
+
+# Run "sed -i -e 's/\r$//' scriptname.sh" command before running script.
+
+# Print end status message.
